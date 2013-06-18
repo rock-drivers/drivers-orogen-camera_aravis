d0d73d83bd2e0325e35f5894c92ff9fd02b7b4a6
Fabian Franzen <flammi@watteimdocht.de>
2013-06-18 02:18:20 +0200

    First working version with camera_aravis lib

--- scripts/test.rb
+++ scripts/test.rb
@@ -6,12 +6,14 @@ Orocos::initialize
 
 Orocos.run "camera_aravis::Task"=> "Task" do
     task = Orocos::TaskContext.get("Task")
-    task.listCameras
     #task.camera_id = "24934672"
-    task.camera_id = "The Imaging Source Europe GmbH-29210317"
-#    task.camera_id = "Fake_1"
+#    task.camera_id = "The Imaging Source Europe GmbH-29210317"
+    #task.camera_id = "Aravis-GV01"
+    task.camera_id = "The Imaging Source Europe GmbH-42210449"
     task.width = 1280
     task.height = 960
+    task.exposure = 10000
+    task.gain = 1
 #    task.width = 640
 #    task.height = 480
     task.configure
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -4,7 +4,6 @@
 #define CAMERA_ARAVIS_TASK_TASK_HPP
 
 #include "camera_aravis/TaskBase.hpp"
-#include <base/samples/frame.h>
 #include <arv.h>
 
 
@@ -27,97 +26,15 @@ namespace camera_aravis {
     class Task : public TaskBase
     {
 	friend class TaskBase;
-    protected:
-        /* Handler for the listCameras operation
-         */
-        virtual ::std::string listCameras();
-        ArvCamera *camera; 
-        ArvStream *stream;
     public:
-        base::Time lastFrameTime;
-	ArvBuffer *last_buffer;
-        base::samples::frame::Frame frame[50];
-        base::samples::frame::Frame *frame_ptr[50];
-        RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> camera_frame[50];
-        bool newFrame; 
-        double exposure,gain;
-        int new_frame_id;
-//        std::vector<uint8_t> local_buffer[50];
-        /** TaskContext constructor for Task
-         * \param name Name of the task. This name needs to be unique to make it identifiable via nameservices.
-         * \param initial_state The initial TaskState of the TaskContext. Default is Stopped state.
-         */
         Task(std::string const& name = "camera_aravis::Task");
-
-        /** TaskContext constructor for Task 
-         * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices. 
-         * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task. 
-         * 
-         */
         Task(std::string const& name, RTT::ExecutionEngine* engine);
-
-        /** Default deconstructor of Task
-         */
 	~Task();
 
-        /** This hook is called by Orocos when the state machine transitions
-         * from PreOperational to Stopped. If it returns false, then the
-         * component will stay in PreOperational. Otherwise, it goes into
-         * Stopped.
-         *
-         * It is meaningful only if the #needs_configuration has been specified
-         * in the task context definition with (for example):
-         \verbatim
-         task_context "TaskName" do
-           needs_configuration
-           ...
-         end
-         \endverbatim
-         */
         bool configureHook();
 
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to Running. If it returns false, then the component will
-         * stay in Stopped. Otherwise, it goes into Running and updateHook()
-         * will be called.
-         */
-        bool startHook();
-
-        /** This hook is called by Orocos when the component is in the Running
-         * state, at each activity step. Here, the activity gives the "ticks"
-         * when the hook should be called.
-         *
-         * The error(), exception() and fatal() calls, when called in this hook,
-         * allow to get into the associated RunTimeError, Exception and
-         * FatalError states. 
-         *
-         * In the first case, updateHook() is still called, and recover() allows
-         * you to go back into the Running state.  In the second case, the
-         * errorHook() will be called instead of updateHook(). In Exception, the
-         * component is stopped and recover() needs to be called before starting
-         * it again. Finally, FatalError cannot be recovered.
-         */
-        void updateHook();
-
-        /** This hook is called by Orocos when the component is in the
-         * RunTimeError state, at each activity step. See the discussion in
-         * updateHook() about triggering options.
-         *
-         * Call recover() to go back in the Runtime state.
-         */
-        // void errorHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Running to Stopped after stop() has been called.
-         */
-        void stopHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to PreOperational, requiring the call to configureHook()
-         * before calling start() again.
-         */
-        // void cleanupHook();
     };
+    void triggerFunction(const void *p);
 }
 
 #endif
